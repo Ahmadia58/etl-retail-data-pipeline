86df1d25c2d94547edab058cb825b732a7cc403b
ahmadia58 <ahmadi.math@gmail.com>
2025-09-26 13:38:07 +0330

    Update extract_orders.py

--- extract/extract_orders.py
+++ extract/extract_orders.py
@@ -11,7 +11,7 @@ logging.info(f"Start of Extraction")
 try:
     df = pd.read_csv("/Users/Ahmad/etl_project/etl-retail-data-pipeline/data_sources/orders_2025_06_06.csv")
     df.to_csv("/Users/Ahmad/etl_project/etl-retail-data-pipeline/extract/extracted_orders.csv", index=False)
-    df.to_parquet("/Users/Ahmad/etl_project/extract/etl-retail-data-pipeline/extracted_orders.parquet", index=False)
+    df.to_parquet("/Users/Ahmad/etl_project/etl-retail-data-pipeline/extract/extracted_orders.parquet", index=False)
     logging.info("Data extracted successfully.")
 except Exception as e:
     logging.error(f"Error in the extraction phase: {e}")
